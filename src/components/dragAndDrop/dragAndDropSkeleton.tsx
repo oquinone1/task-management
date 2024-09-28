39c935dc27c058b6acbaf2932aa03153b8fc21c5
Omar <account1@omars-mbp.attlocal.net>
2024-09-27 17:06:12 -0700

    removed the active state and stle of drag and drop skeleton

--- src/components/dragAndDrop/dragAndDropSkeleton.tsx
+++ src/components/dragAndDrop/dragAndDropSkeleton.tsx
@@ -1,19 +1,23 @@
 import SkeletonAntd from "../antd components/skeletonAntd";
 
 const DragAndDropSkeletonComponent = () => {
-  const columns = [...Array(4)].map((_: any, index: number) => {
-    return (
-      <SkeletonAntd
-        key={`skeleton_${index}`}
-        style={{ minWidth: "250px" }}
-        paragraph={{ rows: 22 }}
-        active
-      />
-    );
-  });
+  // const columns = [...Array(4)].map((_: any, index: number) => {
+  //   return (
+  //     <SkeletonAntd
+  //       key={`skeleton_${index}`}
+  //       style={{ minWidth: "250px" }}
+  //       paragraph={{ rows: 22 }}
+  //       active
+  //     />
+  //   );
+  // });
   return (
     <section className="flex flex-row justify-between h-full mr-[20px] pt-[20px]">
-      {columns}
+      <SkeletonAntd
+        // style={{ minWidth: "250px" }}
+        paragraph={{ rows: 20 }}
+        // active
+      />
     </section>
   );
 };
